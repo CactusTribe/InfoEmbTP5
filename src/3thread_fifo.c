47ebf1ecbfd4ca1e22be448c96b798d8941835e8
CactusTribe <lefrancjoaquim@gmail.com>
2017-12-05 17:59:10 +0100

    Ajout de l'affichage

--- src/3thread_fifo.c
+++ src/3thread_fifo.c
@@ -6,12 +6,17 @@
 #include <time.h>
 #include <semaphore.h>
 
-#define PRIORITY_T1 51
-#define PRIORITY_T2 21
-#define PRIORITY_T3 1
+#define _GNU_SOURCE
+
+#define PRIORITY_T1 30
+#define PRIORITY_T2 20
+#define PRIORITY_T3 10
 
 pthread_mutex_t verrou;
 sem_t sem1,sem2,sem3;
+
+int medium_executed = 0;
+int inversion = 0;
 
 void Thread1()
 {
@@ -20,6 +25,7 @@ void Thread1()
   printf("Thread_1 : HIGH Pritority\n");
   sem_post(&sem3);
   pthread_mutex_unlock(&verrou);
+  if(medium_executed) inversion = 1;
 }
 
 void Thread2()
@@ -27,8 +33,8 @@ void Thread2()
   sem_wait(&sem2);
   pthread_mutex_lock(&verrou);
   printf("Thread_2 : MEDIUM Pritority\n");
-  printf(" >> Inversion détectée <<\n");
   pthread_mutex_unlock(&verrou);
+  medium_executed = 1;
 }
 
 void Thread3()
@@ -57,27 +63,27 @@ int main(int argc, char* argv[])
   pthread_attr_init(&attr2);
   pthread_attr_init(&attr3);
 
-  int i;
+  int policy;
   if(argc > 1){
-      i = PTHREAD_INHERIT_SCHED;
+      policy = PTHREAD_INHERIT_SCHED;
   }else{
-      i = PTHREAD_EXPLICIT_SCHED;
+      policy = PTHREAD_EXPLICIT_SCHED;
   }
 
   param.sched_priority = PRIORITY_T1;
   pthread_attr_setschedpolicy(&attr1,SCHED_FIFO);
   pthread_attr_setschedparam(&attr1,&param);
-  pthread_attr_setinheritsched(&attr1,i);
+  pthread_attr_setinheritsched(&attr1, policy);
 
   param.sched_priority = PRIORITY_T2;
   pthread_attr_setschedpolicy(&attr2,SCHED_FIFO);
   pthread_attr_setschedparam(&attr2,&param);
-  pthread_attr_setinheritsched(&attr2,i);
+  pthread_attr_setinheritsched(&attr2, policy);
 
   param.sched_priority = PRIORITY_T3;
   pthread_attr_setschedpolicy(&attr3,SCHED_FIFO);
   pthread_attr_setschedparam(&attr3,&param);
-  pthread_attr_setinheritsched(&attr3,i);
+  pthread_attr_setinheritsched(&attr3, policy);
 
   pthread_create(&ppid1,&attr1,(void *)Thread1, NULL);
   pthread_create(&ppid2,&attr2,(void *)Thread2, NULL);
@@ -90,5 +96,12 @@ int main(int argc, char* argv[])
   pthread_attr_destroy(&attr1);
   pthread_attr_destroy(&attr2);
   pthread_attr_destroy(&attr3);
+
+  if(inversion){
+    printf("%s\n", "Inversion détectée");
+  }else{
+    printf("%s\n", "Inversion non détectée");
+  }
+
   return 0;
 }
