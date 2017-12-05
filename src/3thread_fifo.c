cda0c847b5d171c31500a56550c096d5c29b8cef
CactusTribe <lefrancjoaquim@gmail.com>
2017-12-05 17:05:35 +0100

    Test

--- src/3thread_fifo.c
+++ src/3thread_fifo.c
@@ -6,35 +6,39 @@
 #include <time.h>
 #include <semaphore.h>
 
+#define PRIORITY_T1 51
+#define PRIORITY_T2 21
+#define PRIORITY_T3 1
+
 pthread_mutex_t verrou;
-//sem_t sem1,sem2,sem3;
-sem_t mutex;
+sem_t sem1,sem2,sem3;
 
 void Thread1()
 {
-  sem_wait(&mutex);
+  sem_wait(&sem1);
   pthread_mutex_lock(&verrou);
   printf("Thread_1 : HIGH Pritority\n");
+  sem_post(&sem3);
   pthread_mutex_unlock(&verrou);
-  //sem_post(&sem1);
 }
 
 void Thread2()
 {
-  sem_wait(&mutex);
+  sem_wait(&sem2);
   pthread_mutex_lock(&verrou);
   printf("Thread_2 : MEDIUM Pritority\n");
+  printf(" >> Inversion détectée <<\n");
   pthread_mutex_unlock(&verrou);
-  //sem_post(&sem2);
 }
 
 void Thread3()
 {
-  sem_wait(&mutex);
+  sem_wait(&sem3);
   pthread_mutex_lock(&verrou);
   printf("Thread_3 : LOW Pritority\n");
+  sem_post(&sem2);
+  sem_post(&sem1);
   pthread_mutex_unlock(&verrou);
-  //sem_post(&sem1);
 }
 
 int main(int argc, char* argv[])
@@ -44,10 +48,9 @@ int main(int argc, char* argv[])
   struct sched_param param;
   pthread_attr_t attr1,attr2,attr3;
   
-  //sem_init(&sem1, 0, 0);
-  //sem_init(&sem2, 0, 0);
-  //sem_init(&sem3, 0, 0);
-  sem_init(&mutex, 0, 0);
+  sem_init(&sem1, 0, 0);
+  sem_init(&sem2, 0, 0);
+  sem_init(&sem3, 0, 1);
 
   pthread_mutex_init(&verrou, NULL);
   
@@ -62,23 +65,20 @@ int main(int argc, char* argv[])
       i = PTHREAD_EXPLICIT_SCHED;
   }
 
-  param.sched_priority = 51;
+  param.sched_priority = PRIORITY_T1;
   pthread_attr_setschedpolicy(&attr1,SCHED_FIFO);
   pthread_attr_setschedparam(&attr1,&param);
   pthread_attr_setinheritsched(&attr1,i);
 
-  param.sched_priority = 21;
+  param.sched_priority = PRIORITY_T2;
   pthread_attr_setschedpolicy(&attr2,SCHED_FIFO);
   pthread_attr_setschedparam(&attr2,&param);
   pthread_attr_setinheritsched(&attr2,i);
  
-  param.sched_priority = 1;
+  param.sched_priority = PRIORITY_T3;
   pthread_attr_setschedpolicy(&attr3,SCHED_FIFO);
   pthread_attr_setschedparam(&attr3,&param);
   pthread_attr_setinheritsched(&attr3,i);
-
-
-  sem_wait(&mutex);
 
   pthread_create(&ppid1,&attr1,(void *)Thread1,NULL);
   pthread_create(&ppid2,&attr2,(void *)Thread2,NULL);
