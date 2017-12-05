52f287f95b6a348dae6c5de9914e188a3b06b4f5
CactusTribe <lefrancjoaquim@gmail.com>
2017-12-05 17:28:18 +0100

    modif

--- Makefile
+++ Makefile
@@ -1,34 +1,25 @@
-# Joaquim Lefranc (21402929)
+# Liang Guan, Joaquim Lefranc
 
 CC = gcc
-CFLAGS = -Wall
-LDFLAGS = -shared
+CFLAGS = -pthread -lrt -std=c11 -Wall
 
 BIN_DIR = bin
 SRC_DIR = src
 LIB_DIR = libs
 HEADER_DIR = header
 
-TPNAME = Lefranc_Joaquim_TP5
-RAPPORT = lefranc_tp5
-EXEC = main
+TPNAME = Guan_Lefranc_TP5
+RAPPORT = guan_lefranc_tp5
+EXEC = 3thread_fifo
 
 EXEC_FILE=$(BIN_DIR)/$(EXEC)
 SRC_FILES = $(wildcard $(SRC_DIR)/**.c)
 OBJ_FILES = $(patsubst %.c, %.o,  $(subst $(SRC_DIR), $(BIN_DIR),$(SRC_FILES)))
 
-UNAME_S := $(shell uname -s)
-ifeq ($(UNAME_S),Linux)
-	LIBFLAGS = 
-endif
-ifeq ($(UNAME_S),Darwin)
-	LIBFLAGS =
-endif
-
 all: $(EXEC_FILE)
 
 $(EXEC_FILE): $(OBJ_FILES)
-	$(CC) $(CFLAGS) -o $@ $^ $(LIBFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 $(OBJ_FILES): $(BIN_DIR)/%.o : $(SRC_DIR)/%.c
 	$(CC) -o $@ -c $< $(CFLAGS)
@@ -37,7 +28,7 @@ run:
 	$(EXEC_FILE) ${args}
 
 clean:
-	rm -rf */*.o */*.a */*.so 
+	rm -rf */*.o */*.a */*.so
 
 mrproper: clean
 	rm -rf $(EXEC_FILE)
--- src/3thread_fifo.c
+++ src/3thread_fifo.c
@@ -43,17 +43,16 @@ void Thread3()
 
 int main(int argc, char* argv[])
 {
-  
   pthread_t ppid1,ppid2,ppid3;
   struct sched_param param;
   pthread_attr_t attr1,attr2,attr3;
-  
+
   sem_init(&sem1, 0, 0);
   sem_init(&sem2, 0, 0);
   sem_init(&sem3, 0, 1);
 
   pthread_mutex_init(&verrou, NULL);
-  
+
   pthread_attr_init(&attr1);
   pthread_attr_init(&attr2);
   pthread_attr_init(&attr3);
@@ -74,15 +73,15 @@ int main(int argc, char* argv[])
   pthread_attr_setschedpolicy(&attr2,SCHED_FIFO);
   pthread_attr_setschedparam(&attr2,&param);
   pthread_attr_setinheritsched(&attr2,i);
- 
+
   param.sched_priority = PRIORITY_T3;
   pthread_attr_setschedpolicy(&attr3,SCHED_FIFO);
   pthread_attr_setschedparam(&attr3,&param);
   pthread_attr_setinheritsched(&attr3,i);
 
-  pthread_create(&ppid1,&attr1,(void *)Thread1,NULL);
-  pthread_create(&ppid2,&attr2,(void *)Thread2,NULL);
-  pthread_create(&ppid3,&attr3,(void *)Thread3,NULL);
+  pthread_create(&ppid1,&attr1,(void *)Thread1, NULL);
+  pthread_create(&ppid2,&attr2,(void *)Thread2, NULL);
+  pthread_create(&ppid3,&attr3,(void *)Thread3, NULL);
 
   pthread_join(ppid1,NULL);
   pthread_join(ppid2,NULL);
